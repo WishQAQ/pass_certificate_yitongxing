ee0f902683e426fddddd6dd09ad7f15429df5a5f
wish <wishtime1025@gmail.com>
2020-02-12 02:27:15 +0800

    新增通信证页面

--- pages/userCenter/index.js
+++ pages/userCenter/index.js
@@ -1,4 +1,6 @@
 // pages/userCenter/index.js
+const app = getApp()
+
 Page({
 
 	/**
@@ -7,36 +9,50 @@ Page({
 	data: {
 		login: false,  // 用户登录状态
 		userInfo: {}, // 用户资料
+		canIUse: wx.canIUse('view.open-type.getUserInfo')
 	},
 
-	loginBtn: function(e) {
-		let showUserInfo = this.data.login
-		this.setData({
-			login: !showUserInfo
-		})
+	getUserInfo: function(e) {
+		app.globalData.userInfo = e.detail.userInfo
+		if (app.globalData.userInfo){
+			this.setData({
+				userInfo: e.detail.userInfo,
+				login: true
+			})
+		}
 	},
 
 	/**
 	 * 生命周期函数--监听页面加载
 	 */
 	onLoad: function (options) {
-		let that = this;
-		wx.getSetting({
-			success(res) {
-				if (res.authSetting['scope.userInfo']) {
-					// 已经授权，可以直接调用 getUserInfo 获取头像昵称
-					wx.getUserInfo({
-						success(res) {
-							console.log(res.userInfo)
-							that.setData({
-								userInfo: res.userInfo
-							})
-						}
+		if (app.globalData.userInfo) {
+			this.setData({
+				userInfo: app.globalData.userInfo,
+				login: true
+			})
+		} else if (this.data.canIUse) {
+			// 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+			// 所以此处加入 callback 以防止这种情况
+			app.userInfoReadyCallback = res => {
+				this.setData({
+					userInfo: res.userInfo,
+					login: true
+				})
+			}
+		} else {
+			// 在没有 open-type=getUserInfo 版本的兼容处理
+			wx.getUserInfo({
+				success: res => {
+					app.globalData.userInfo = res.userInfo
+					this.setData({
+						userInfo: res.userInfo,
+						login: true
 					})
 				}
-			}
-		})
-	},
+			})
+		}
+  },
 
 	/**
 	 * 生命周期函数--监听页面初次渲染完成
